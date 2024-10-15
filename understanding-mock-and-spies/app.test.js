97a3af7ecdedbfc2768ac8cd6bee1f1fb8e309ad
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-10-15 12:32:48 +0300

    added spy function to the generate token

--- understanding-mock-and-spies/app.test.js
+++ understanding-mock-and-spies/app.test.js
@@ -1,10 +1,11 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { generateToken } from "./app";
 
-
-
+// spy on the log function
 describe("generateToken", () => {
     it("should execute if logger function is provided", () => {
-        generateToken()
+        const loggerSpyFn = vi.fn(); // create a spy function
+        generateToken(loggerSpyFn);
+        expect(loggerSpyFn).toHaveBeenCalled();
     });
 });
